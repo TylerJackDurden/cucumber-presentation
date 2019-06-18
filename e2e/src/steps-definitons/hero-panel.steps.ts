13c192b3930ec1aed6a047eb964926bfd59e4fd8
Dominik Pettke <pettke.dominik@gmail.com>
2019-06-18 10:51:28 +0200

    step first no parameters no scenario outline

--- e2e/src/steps-definitons/hero-panel.steps.ts
+++ e2e/src/steps-definitons/hero-panel.steps.ts
@@ -17,6 +17,14 @@ Given("{string} page is open", async (pageName: string) => {
     }
 });
 
+Given("heroes page is open", async (pageName: string) => {
+    await browser.get('heroes');
+});
+
+Given("dashboard page is open", async () => {
+    await browser.get('dashboard');
+});
+
 When(/^I click on Heroes button$/, async () => {
     await heroPanel.getHeroesButton().click();
 
@@ -26,6 +34,10 @@ When(/^I click on "(.*)" hero$/, async (hero: string) => {
   await heroPanel.clickOnSpecyficHero(hero.toLowerCase()).click();
 });
 
+When(/^I click on Magneta hero$/, async () => {
+    await heroPanel.clickOnSpecyficHero('magneta').click();
+  });
+
 Then(/^I can see list of heroes$/, async () => {
     expect(await heroPanel.heroesList.isPresent()).to.equal(true);
 });
@@ -34,8 +46,16 @@ Then(/^I see "([^"]*)" details$/, async (hero: string) => {
   expect(await heroPanel.detailOfhero.getText()).to.equal(hero.toLocaleUpperCase() + ' Details');
 });
 
+Then(/^I see Magneta details$/, async () => {
+    expect(await heroPanel.detailOfhero.getText()).to.equal('MAGNETA' + ' Details');
+  });
+
 Then(/^I see "([^"]*)" id$/, async (heroId: number) => {
     expect(await heroPanel.detailOfHeroID.getText()).to.equal(heroId.toString());
+  });
+
+  Then(/^I see 15 id$/, async () => {
+    expect(await heroPanel.detailOfHeroID.getText()).to.equal('15');
   });
 
 
